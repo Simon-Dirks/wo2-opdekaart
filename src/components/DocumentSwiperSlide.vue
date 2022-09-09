229c1e44db753e9e1a85dd370537e37e4e909e2d
Simon <Simon-Dirks@users.noreply.github.com>
2022-09-09 14:19:22 +0200

    Scroll through document people

--- src/components/DocumentSwiperSlide.vue
+++ src/components/DocumentSwiperSlide.vue
@@ -1,6 +1,7 @@
 <template>
   <div :class="[isShownFullscreen ? 'grid grid-cols-6 text-white grid-flow-dense': '']" v-if="document">
-    <div class="col-span-6 h-full flex-initial" :class="[documentRefersToPeople ? 'md:col-start-3 md:col-span-4' : '' ]">
+    <div class="col-span-6 h-full flex-initial"
+         :class="[documentRefersToPeople ? 'md:col-start-3 md:col-span-4' : '' ]">
       <div class="flex-auto">
         <button @click="openModal" class="w-full" :class="isShownFullscreen ? 'cursor-default' : 'cursor-pointer'">
           <!-- TODO: Add pinch to zoom-->
@@ -27,14 +28,17 @@
         </button>
       </div>
       <div class="text-center drop-shadow flex-[0_1_20px]" v-if="isShownFullscreen">
-        <p>{{ document.label }}
+        <p>
+          <a :href="document.id" target="_blank"> {{ document.label }}</a>
         </p>
-        <!--      <p><a :href="document.url" target="_blank">LINK</a></p>-->
-        <!--      <p>{{ document.source.id }}</p>-->
+        <p>
+          Bron: {{document.source.label}}
+        </p>
       </div>
     </div>
 
-    <div class="col-span-6 max-h-[90vh] overflow-y-auto flex-initial md:col-span-2" v-if="isShownFullscreen && documentRefersToPeople">
+    <div class="col-span-6 max-h-[90vh] overflow-y-auto flex-initial md:col-span-2"
+         v-if="isShownFullscreen && documentRefersToPeople">
       <h2 class="text-2xl">Personen</h2>
       <ul class="list-disc">
         <li v-for="person in props.document.people">{{ person.label }}</li>
