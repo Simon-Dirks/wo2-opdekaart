f6412afb06306c42255008096974fe0d9a853dc3
Simon <Simon-Dirks@users.noreply.github.com>
2022-09-09 12:43:05 +0200

    Add TODO

--- src/components/SourceSelect.vue
+++ src/components/SourceSelect.vue
@@ -3,6 +3,7 @@ import {SourceModel} from "../models/source.model";
 import {onMounted, Ref, ref} from "vue";
 import {useStore} from "vuex";
 import {MapService} from "../services/map.service";
+import {useLoadingBar} from "naive-ui";
 
 const sources: SourceModel[] = [
   {"id": "https://hetutrechtsarchief.nl/id/Inkwartiering", "label": "Inkwartiering"},
@@ -17,15 +18,17 @@ onMounted(() => {
   store.commit("setShownSourceIds", selectedSourceIds);
 });
 
-const onSourceSelect = (sourceId: string, isSelected: boolean) => {
-    if(isSelected) {
-      selectedSourceIds.value.add(sourceId);
-    } else {
-      selectedSourceIds.value.delete(sourceId);
-    }
+const onSourceSelect = async (sourceId: string, isSelected: boolean) => {
+  if (isSelected) {
+    selectedSourceIds.value.add(sourceId);
+  } else {
+    selectedSourceIds.value.delete(sourceId);
+  }
 
-    store.commit("setShownSourceIds", selectedSourceIds);
-    new MapService().updateFilter();
+  store.commit("setShownSourceIds", selectedSourceIds);
+
+  // TODO: Add loading bar
+  await new MapService().updateFilter();
 }
 </script>
 
@@ -35,12 +38,14 @@ const onSourceSelect = (sourceId: string, isSelected: boolean) => {
       <n-collapse>
         <n-collapse-item title="Bronnen" class="pr-4">
           <div v-for="source in sources">
-            <n-checkbox :id="source.id" :checked="selectedSourceIds.has(source.id)" @update:checked="onSourceSelect(source.id, $event)">
+            <n-checkbox :id="source.id" :checked="selectedSourceIds.has(source.id)"
+                        @update:checked="onSourceSelect(source.id, $event)">
               {{ source.label }}
             </n-checkbox>
           </div>
         </n-collapse-item>
-      </n-collapse>  </n-card>
+      </n-collapse>
+    </n-card>
   </div>
 </template>
 
