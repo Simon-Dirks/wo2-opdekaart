8cd98e368c613f7246bb959512b76f95c9d9107e
Simon <Simon-Dirks@users.noreply.github.com>
2022-09-12 12:22:00 +0200

    Update LandingPage.vue

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -4,8 +4,11 @@
 
 <template>
   <div class="h-screen w-screen p-4">
-    <h1 class="text-2xl">WO2 op de kaart</h1>
-
-    <n-button type="primary" class="mt-4" @click="$router.push('map')">Naar de kaart!</n-button>
+    <h1 class="text-2xl mb-2">WO2 op de kaart</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad culpa cumque debitis, delectus dicta doloremque
+      doloribus eius eveniet fugit illum ipsum, labore laborum maiores nulla provident quo reiciendis rem, vitae?</p>
+    <button class="mt-4 bg-primary px-4 py-2 rounded-full text-white hover:bg-red-800 transition-colors"
+            @click="$router.push('map')">Naar de kaart!
+    </button>
   </div>
 </template>
