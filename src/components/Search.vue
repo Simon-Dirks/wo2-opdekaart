fea43ca9bbb8073b608401f4bf6c562322ebbe09
Simon <Simon-Dirks@users.noreply.github.com>
2022-10-06 15:25:24 +0200

    Update search layout

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,62 +1,83 @@
 <script setup lang="ts">
-import {MapService} from "../services/map.service";
-import {onMounted, Ref, ref, watch} from 'vue'
-import {store} from "../store";
+import { MapService } from "../services/map.service";
+import { onMounted, Ref, ref, watch } from "vue";
+import { store } from "../store";
 
 const mapService: MapService = new MapService();
-const search: Ref<string> = ref('');
-const radioOption: Ref<string> = ref('all');
+const search: Ref<string> = ref("");
+const radioOption: Ref<string> = ref("all");
 
 const radioOptions = [
   {
     value: "all",
-    label: "Alles"
+    label: "Alles",
   },
   {
-    value: 'people',
-    label: 'Personen'
+    value: "people",
+    label: "Personen",
   },
   {
-    value: 'addresses',
-    label: 'Adressen'
-  }
+    value: "addresses",
+    label: "Adressen",
+  },
 ];
 
 const onSearchButtonClicked = () => {
-  console.log("onUpdate", search)
+  console.log("onUpdate", search);
   store.commit("setSearchTerm", search.value);
-}
+};
 
 onMounted(() => {
-  watch(() => store.getters["getSearchTerm"], (searchTerm: string) => {
-    search.value = searchTerm;
-    console.log(search);
-  });
+  watch(
+    () => store.getters["getSearchTerm"],
+    (searchTerm: string) => {
+      search.value = searchTerm;
+      console.log(search);
+    }
+  );
 });
-
 </script>
 
 <template>
   <div class="drop-shadow">
-    <n-input type="text" class="rounded-lg" placeholder="Zoeken" v-model:value="search"/>
-    <n-button class="bg-[rgba(255,255,255,0.8)] px-4 py-2 rounded-lg" @click="onSearchButtonClicked">Zoeken</n-button>
-    <br/>
+    <div class="mb-1">
+      <n-input
+        type="text"
+        class="rounded-lg mr-2"
+        placeholder="Zoeken"
+        v-model:value="search"
+      />
 
-    <n-radio-group v-model:value="radioOption" name="radiogroup" class="bg-[rgba(255,255,255,0.8)] px-4 py-2 rounded-lg">
+      <n-button
+        class="bg-[rgba(255,255,255,0.8)] px-4 py-2 rounded-lg inline-block"
+        @click="onSearchButtonClicked"
+      >
+        Zoeken
+      </n-button>
+    </div>
+
+    <n-radio-group
+      v-model:value="radioOption"
+      name="radiogroup"
+      class="bg-[rgba(255,255,255,0.8)] px-4 py-2 rounded-lg"
+    >
       <n-space>
         <n-radio
-            v-for="option in radioOptions"
-            :key="option.value"
-            :value="option.value"
-            :label="option.label"
+          v-for="option in radioOptions"
+          :key="option.value"
+          :value="option.value"
+          :label="option.label"
         />
       </n-space>
     </n-radio-group>
 
-<!--    <n-input size="large" round placeholder="Zoeken..." v-model.trim="search"  v-debounce="onUpdate"/>-->
-
+    <!--    <n-input size="large" round placeholder="Zoeken..." v-model.trim="search"  v-debounce="onUpdate"/>-->
   </div>
 </template>
 
 <style scoped>
+.n-input {
+  display: inline-block;
+  width: 15rem;
+}
 </style>
