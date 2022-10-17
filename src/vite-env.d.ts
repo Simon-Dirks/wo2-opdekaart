b8cd90dbde408f68a7ebd1affc4f0b320afb0d2e
Rick Companje <rick@companje.nl>
2022-10-17 20:00:41 +0200

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,15 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_TRIPLY_TOKEN: string
+  // readonly VITE_TRIPLY_TOKEN: string
+  readonly VITE_RESULTS_PER_PAGE: number;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
