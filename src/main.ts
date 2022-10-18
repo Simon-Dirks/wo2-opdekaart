5b4a32d7901d0f37487cce9c6f464d7fd0d4382f
Simon <Simon-Dirks@users.noreply.github.com>
2022-10-18 14:58:01 +0200

    Reformat main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import {createApp} from 'vue'
-import './index.css'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./index.css";
+import "./style.css";
+import App from "./App.vue";
 import VueMapboxTs from "vue-mapbox-ts";
-import {store} from "./store";
+import { store } from "./store";
 import naive from "naive-ui";
-import {Icon, IconConfigProvider} from "@vicons/utils";
+import { Icon, IconConfigProvider } from "@vicons/utils";
 import router from "./router";
-import VueLazyLoad from 'vue3-lazyload'
-import {vue3Debounce} from "vue-debounce";
-import 'viewerjs/dist/viewer.css'
-import VueViewer from 'v-viewer'
+import VueLazyLoad from "vue3-lazyload";
+import { vue3Debounce } from "vue-debounce";
+import "viewerjs/dist/viewer.css";
+import VueViewer from "v-viewer";
 
 const app = createApp(App);
 app.use(router);
@@ -18,16 +18,17 @@ app.use(store);
 app.use(VueMapboxTs);
 app.use(naive);
 app.use(VueLazyLoad, {
-    loading: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif',
-    error: 'https://via.placeholder.com/350x150',
-    lifecycle: {
-        error: (el: any) => {
-            // console.warn(el);
-        }
-    }
+  loading:
+    "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif",
+  error: "https://via.placeholder.com/350x150",
+  lifecycle: {
+    error: (el: any) => {
+      // console.warn(el);
+    },
+  },
 });
 app.use(VueViewer);
-app.component('Icon', Icon);
-app.component('IconConfigProvider', IconConfigProvider);
-app.directive('debounce', vue3Debounce({lock: true}));
-app.mount('#app');
+app.component("Icon", Icon);
+app.component("IconConfigProvider", IconConfigProvider);
+app.directive("debounce", vue3Debounce({ lock: true }));
+app.mount("#app");
