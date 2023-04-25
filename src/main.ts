0574f2cb3288e7464b56b106a62d68509cbb69c6
Simon <Simon-Dirks@users.noreply.github.com>
2023-04-25 10:38:01 +0200

    Refer to image as an imported asset instead of through URL

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import VueLazyLoad from "vue3-lazyload";
 import { vue3Debounce } from "vue-debounce";
 import "viewerjs/dist/viewer.css";
 import VueViewer from "v-viewer";
+import imageNotVisiblePlaceholder from "./assets/afbeelding-niet-zichtbaar.png";
 
 const app = createApp(App);
 app.use(router);
@@ -20,8 +21,8 @@ app.use(naive);
 app.use(VueLazyLoad, {
   loading:
     "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif",
-    // error: "https://via.placeholder.com/350x150",
-    error: "https://wo2kaart.hetutrechtsarchief.nl/assets/afbeelding-niet-zichtbaar.png",
+  // error: "https://via.placeholder.com/350x150",
+  error: imageNotVisiblePlaceholder,
   lifecycle: {
     error: (el: any) => {
       // console.warn(el);
